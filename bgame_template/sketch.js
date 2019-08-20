fb849b0150d295bf5aa1d7dbfb3c504fa282baf7
DeadAugust <augustluhrs@gmail.com>
2019-08-20 16:49:30 -0700

    working board draw + click

--- bgame_template/sketch.js
+++ bgame_template/sketch.js
@@ -4,66 +4,26 @@
   Summer 2019
 */
 
-<<<<<<< HEAD
 //games (bY - rows, bX - cols)
 let ttt = {
-  bY: 3,
-  bX: 3,
+  bRows: 3,
+  bCols: 3,
   p1: 'X',
-  p2: 'O'
+  p2: 'O',
+  winCheck: function(game){
+    for (let i = 0; i < game.board.grid.length; i++){
+      // console.log('win!');
+
+    }
+  }
 }
 
 let c4 = {
-  bY: 6,
-  bX: 7,
+  bRows: 6,
+  bCols: 7,
   p1: 'red',
   p2: 'yellow'
 };
-
-class Square{
-  constructor(rows, cols, gridY, gridX, cornerY, cornerX){ //rows, cols
-    // this.gridY = gridY;
-    // this.gridX = gridX;
-    // this.cornerY = height/rows;
-    // this.cornerX = width/cols;
-    this.centerY = cornerY + ((gridY+1)*(squareSize/2));
-    this.centerX = cornerX + ((gridX+1)*(squareSize/2));
-
-
-    this.clicked = function(){ //need to be in constructor???
-      let d = dist(mouseX, mouseY, this.centerX, this.centerY);
-
-      if (d<(squareSize/2)){
-        console.log(this.centerX, this.centerY);
-        return true;
-      }
-    }
-  }
-}
-
-class Board{
-  constructor(bY, bX){
-    this.rows = bY;
-    this.cols = bX;
-    this.cornerY = height/this.rows;
-    this.cornerX = width/this.cols;
-    let grid = [];
-    for (let i = 0; i < this.rows; i++){
-      grid[i] = [];
-      for (let j = 0; j < this.cols; j++){
-        // grid[i].push('');
-        let newSq = new Square(this.rows, this.cols, i, j, this.cornerY, this.cornerX);
-        grid[i].push(newSq);
-      }
-    }
-    this.grid = grid;
-  }
-  move(){
-    // console.log(this.grid);
-    // console.log(game.board);
-    // console.log(bX, bY);
-  }
-}
 
 //overall variables
 let game = {
@@ -73,99 +33,159 @@ let game = {
 // let gameBoard = ttt;
 let gameSelect;
 let reset;
-let firstPlayer;
+// let firstPlayer;
+let currentPlayer;
 let gameStarted = false;
 // let firstMove = false;
-// let squareSize = 50;
-let squareSize;
-let boardSquares = []; //new attempt at reverse way of doing board/squares
+let squareSize; //need to make a part of board variable so bigger board still small
+let wideScreen;
+//board stats same for all games:
+let boardWidth, boardLength;
+let boardCenterX, boardCenterY;
+let boardCornerX, boardCornerY;
+let win = false;
 
 
+class Square {
+  constructor(bCols, bRows, index, gridX, gridY) { //
+    this.bCols = bCols;
+    this.bRows = bRows;
+    this.index = index;
+    this.gridX = gridX;
+    this.gridY = gridY;
+
+    //the center of each square
+    // this.centerY = cornerY + ((gridY+1)*(squareSize/2));
+    // this.centerX = cornerX + ((gridX+1)*(squareSize/2));
+
+    this.centerX = boardCornerX + (gridX * squareSize) + squareSize / 2;
+    this.centerY = boardCornerY + (gridY * squareSize) + squareSize / 2;
+
+
+    //what's in it
+    this.slot = '';
+  }
+  clicked() {
+    let d = dist(mouseX, mouseY, this.centerX, this.centerY);
+
+    if (d < (squareSize / 2)) {
+      console.log(this.index, this.slot, this.centerX, this.centerY);
+      if (this.slot == '') {
+        return true;
+      }
+    }
+  }
+}
+
+class Board {
+  constructor(cols, rows) {
+    this.cols = cols;
+    this.rows = rows;
+
+    // relative size of board/squares and text
+    squareSize = boardWidth / rows; //matters?
+    textSize(4 * squareSize / 5);
+
+    //top left corner of board
+    boardCornerX = (width/2) - (boardWidth / 2);
+    boardCornerY = (height/2) - (boardHeight / 2);
+
+
+    //set up array of squares
+    let grid = [];
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        let index = (y * rows) + x;
+
+        // let newSq = new Square (cols, rows, index, x, y, centerX, centerY);
+        let newSq = new Square(cols, rows, index, x, y);
+        console.log(newSq);
+        grid.push(newSq);
+        //hmm....
+      }
+    }
+    this.grid = grid;
+  }
+  draw() { //display game
+    for (let i = 0; i < this.cols * this.rows; i++) {
+      rect(this.grid[i].centerX, this.grid[i].centerY, squareSize, squareSize);
+      text(this.grid[i].slot, this.grid[i].centerX, this.grid[i].centerY);
+      //need eventual color option for C4
+    }
+  }
+  move() { //click on board
+    for (let i = 0; i < this.cols * this.rows; i++) {
+      if (this.grid[i].clicked()) {
+        this.grid[i].slot = currentPlayer;
+        //change player, next turn
+        if (currentPlayer == game.params.p1) {
+          console.log('player 2 turn');
+          currentPlayer = game.params.p2;
+        } else {
+          console.log('player 1 turn');
+          currentPlayer = game.params.p1;
+        }
+      }
+    }
+  }
+}
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  textAlign(CENTER, CENTER);
 
-  if (windowWidth < windowHeight){
-    squareSize = windowWidth/10;
+  if (windowWidth < windowHeight) {
+    // squareSize = windowWidth/10;
+    wideScreen = false;
+    boardWidth = width / 2;
+    boardHeight = boardWidth;
   } else {
-    squareSize = windowHeight/10;
+    // squareSize = windowHeight/10;
+    wideScreen = true;
+    boardHeight = height / 2;
+    boardWidth = boardHeight;
   }
+  //center of board, in middle of canvas
+  // boardCenterX = width/2;
+  // boardCenterY = height/2;
 
   //game dropdown
   gameSelect = createSelect();
-  gameSelect.position(width/2, height/20);
+  gameSelect.position(width / 2, height / 20);
   gameSelect.option('tic tac toe');
   gameSelect.option('connect 4');
   // gameSelect.changed(changeGame);
 
   //reset game
   reset = createButton('New Game');
-  reset.position(width/2, height/10);
+  reset.position(width / 2, height / 10);
   reset.mousePressed(resetGame);
 
+  resetGame(); //starts with ttt
 }
 
 function draw() {
-  // background(220);
+  background(220);
   push();
   textSize(20);
-  if (gameStarted){
-    text('Player Turn: ' + firstPlayer, width/2, 2 * height/10);
+  if (gameStarted) {
+    text('Player Turn: ' + currentPlayer, width / 2, 2 * height / 10);
   }
   pop();
-  drawBoard();
+  // drawBoard();
+  game.board.draw();
 }
 
-function mousePressed(){
+function mousePressed() {
   //need to make square class with clickable thing
-  // game.board.move();
-  console.log('XY: ', mouseX, mouseY);
-  console.log('SS', squareSize);
-  console.log('Corn:', game.board.cornerX, game.board.cornerY);
-  for (let y = 0; y < game.board.cols; y++){
-    // console.log(game.board.grid[y][0]);
-
-    for (let x = 0; x < game.board.rows; x++){
-      ellipse(game.board.grid[y][x].centerX, game.board.grid[y][x].centerY, squareSize/2, squareSize/2);
-      if (game.board.grid[y][x].clicked()){
-        console.log('row/col', y, x);
-=======
-let board = [
-  ['X', 'O', 'O'],
-  ['', 'X', ''],
-  ['', 'O', ''],
-];
-
-let player1 = 'X';
-let player2 = '0';
-
-function setup(){
-  createCanvas(400,400);
-
+  game.board.move();
+  // console.log('XY: ', mouseX, mouseY);
+  // console.log('SS', squareSize);
+  // console.log('Corn:', game.board.cornerX, game.board.cornerY);
 }
-
-function draw(){
-  background(220);
-  let w = width/3;
-  let h = height/3;
-
-  for (let i = 0; i < 3; i++){
-    for (let j = 0; j < 3; j++){
-      let x = w * i + w/2;
-      let y = h * j + h/2;
-      let spot = board[i][j];
-      if (spot == player1){
-        ellipse(x,y,w);
-      } else if (spot == player 2) {
-        line(x, y, x+w, y+h);
-        line(x+w, y, x, y+h);
->>>>>>> 3f3bae517a39d6f6df985234e1ed066abdcf372f
-      }
-    }
-  }
-}
-<<<<<<< HEAD
+/*
 function drawBoard(){
 
   // push();
@@ -178,24 +198,22 @@ function drawBoard(){
   }
   // pop();
 }
-50
-function resetGame(){
+*/
+function resetGame() {
   gameStarted = true;
   //change game data
   game.name = gameSelect.value();
-  if(game.name == 'tic tac toe'){
+  if (game.name == 'tic tac toe') {
     game.params = ttt;
-  } else if (game.name == 'connect 4'){
+  } else if (game.name == 'connect 4') {
     game.params = c4;
   }
-  game.board = new Board(game.params.bY, game.params.bX);
+  game.board = new Board(game.params.bCols, game.params.bRows);
   //choose random first player
   let r = random([0, 1]);
-  if (r == 0){
-    firstPlayer = game.params.p1;
+  if (r == 0) {
+    currentPlayer = game.params.p1;
   } else {
-    firstPlayer = game.params.p2;
+    currentPlayer = game.params.p2;
   }
 }
-=======
->>>>>>> 3f3bae517a39d6f6df985234e1ed066abdcf372f
